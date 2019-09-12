40a00dc16e2dc11e3507eb489de3b1dc71fafd3a
Heberth Reinstein <heberth.reinstein@gmail.com>
2019-09-12 13:37:10 -0300

    puzzle service

--- src/app/services/puzzle.service.ts
+++ src/app/services/puzzle.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AuthenticationService } from './authentication.service';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Storage } from '@ionic/storage';
 
 @Injectable({
   providedIn: 'root'
@@ -8,20 +9,34 @@ import { AngularFirestore } from '@angular/fire/firestore';
 export class PuzzleService {
 
   constructor(private auth: AuthenticationService,
-              private afs: AngularFirestore) { }
+              private afs: AngularFirestore,
+              private storage: Storage) { }
 
   /**
    * Get the puzzles of the loged user
    */
   getUsersPuzzles() {
-   return this.afs.collection('puzzle').valueChanges();
+    const arrayRet = [];
+    const i = 0;
+    this.storage.get('uid').then(uid => {
+      const docs = this.afs.collection('user_puzzle', pzz => pzz.where('uid', '==', uid));
+      docs.valueChanges().forEach( col => {
+        col.forEach( userPuzzle => {
+          this.afs.doc(userPuzzle.puzzle).valueChanges().forEach(res => {
+            arrayRet.push(res);
+          });
+        });
+      });
+    });
+
+    return arrayRet;
   }
 
   /**
    * Get a puzzle by the title
    * @param title Puzzle title
    */
-  getPuzzleDetails(title: string) {
+getPuzzleDetails(title: string) {
     return this.afs.collection('puzzles', loc => loc.where('title', '==', title));
   }
 
