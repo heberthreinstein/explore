f65ccc3f7263ef17f066c6dce3996aff7db5a7b9
Heberth Reinstein <heberth.reinstein@gmail.com>
2020-07-25 13:56:52 +0000

    veryfing correct answer, saving history

--- src/app/members/quiz/quiz.page.ts
+++ src/app/members/quiz/quiz.page.ts
@@ -1,48 +1,101 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { QuizService } from 'src/app/services/quiz.service';
-import { AuthenticationService } from 'src/app/services/authentication.service';
-import { element } from 'protractor';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { QuizService } from "src/app/services/quiz.service";
+import { AuthenticationService } from "src/app/services/authentication.service";
+import { element } from "protractor";
+import { AlertaService } from "src/app/services/alert.service";
 
 @Component({
-  selector: 'app-quiz',
-  templateUrl: './quiz.page.html',
-  styleUrls: ['./quiz.page.scss'],
+  selector: "app-quiz",
+  templateUrl: "./quiz.page.html",
+  styleUrls: ["./quiz.page.scss"],
 })
 export class QuizPage implements OnInit {
-
   location;
   quiz = new Array();
-  question
+  question: any = "";
+  options = new Array();
+  loading;
 
-  constructor(private activRouter: ActivatedRoute,
-              private quizService: QuizService,
-              private auth: AuthenticationService
-     ) { }
+  constructor(
+    private activRouter: ActivatedRoute,
+    private quizService: QuizService,
+    private auth: AuthenticationService,
+    private alert: AlertaService
+  ) {}
 
-   async ngOnInit() {
-        this.location = this.activRouter.snapshot.paramMap.get('location');
-        await this.quizService.getQuizByLocation(this.location).get().subscribe(res => {res.forEach(element => {
-               this.quiz.push(element.data());
-            }); 
-        }); 
-        this.getActualQuestion();
+  async ngOnInit() {
+    this.loading = await this.alert.loading();
+    this.location = this.activRouter.snapshot.paramMap.get("location");
+    this.quizService
+      .getQuizByLocation(this.location)
+      .get()
+      .subscribe((res) => {
+        res.forEach((element) => {
+          this.quiz.push(element.data());
+        });
+      });
+    this.getActualQuestion();
+  }
+
+  getActualQuestion() {
+    return this.quizService
+      .getUserLastQuestion(
+        this.auth.getLogedUserInformations().uid,
+        this.location
+      )
+      .subscribe((res: any) => {
+        console.log("res", res);
+        if (res.length == 0) {
+          this.getQuestion(0);
+        } else {
+          const actualQuestion = res[0].order + 1;  
+          console.log('acq', actualQuestion)
+          this.getQuestion(actualQuestion);
+        }
+      });
+  }
+
+  getQuestion(actualQuestion) {
+    console.log('quiz',this.quiz)
+    this.quiz.forEach(element => {
+        if (element.order == actualQuestion) {
+            this.question = element;
+        }
+    });
+    console.log('qst', this.question)
+    this.options = this.question.options;
+    this.shuffleOptions();
+    this.loading.dismiss();
+  }
+
+  shuffleOptions() {
+    var currentIndex = this.options.length,
+      temporaryValue,
+      randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = this.options[currentIndex];
+      this.options[currentIndex] = this.options[randomIndex];
+      this.options[randomIndex] = temporaryValue;
     }
+  }
 
-    async getActualQuestion(){
-        var lastQuestion;
-        return await this.quizService.getUserLastQuestion(this.auth.getLogedUserInformations().uid, this.location).subscribe(res => { 
-            console.log('res',res);
-            if (res.length == 0) {
-                this.getQuestion(0);
-            } else {
-                
-            }
-        });        
-
+  verifyAnswer(answer) {
+    if (answer == this.question.answer) {
+      if (this.question.order == 0) {
+          this.quizService.setUserLastQuestion(this.auth.getLogedUserInformations().uid, this.question.location ,this.question.order);
+      } else {
+          this.quizService.updateUserLastQuestion(this.auth.getLogedUserInformations().uid, this.question.location ,this.question.order);   
+      }
+    } else {
+      this.alert.alert("Respota Incorreta");
     }
-
-    getQuestion(actualQuestion){
-        this.question = this.quiz[actualQuestion];
-    }
+  }
 }
--- src/app/services/quiz.service.ts
+++ src/app/services/quiz.service.ts
@@ -6,10 +6,37 @@ import { AngularFirestore } from '@angular/fire/firestore';
 })
 export class QuizService {
 
+    
     constructor(private afs: AngularFirestore) { }
-
+    
     quizCollection = this.afs.collection('quiz');
+    
+    updateUserLastQuestion(uid: string, location: any, order: number) {
+        const userQuiz = {
+            uid: uid,
+            location: location,
+            order: order
+        };
+        this.afs.collection('userQuiz').snapshotChanges().subscribe(res => (
+            res.forEach(item => {
+                const ulq: any = item.payload.doc.data();
+                if (ulq.uid == uid && ulq.location == location) {
+                    this.afs.collection('userQuiz').doc(item.payload.doc.id.toString()).update(userQuiz);
+                }
+            }
+            )
+        ));
+    }
 
+    setUserLastQuestion(uid: string, location: string, order: number) {
+        const userQuiz = {
+            uid: uid,
+            location: location,
+            order: order
+        };
+        this.afs.collection('userQuiz').add(userQuiz);
+    }
+    
     getQuizByLocation(location: String){
         return this.afs.collection('question', q => q.where('location', '==', location));
     }
